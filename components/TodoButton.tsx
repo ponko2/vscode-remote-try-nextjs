318ad26e53d5952d9c8935e5788ea241e34fb10c
Takahito Nakano <kano@ponko2.jp>
2024-05-02 14:18:54 +0900

    refactor: より短く書ける型に変更

--- components/TodoButton.tsx
+++ components/TodoButton.tsx
@@ -2,10 +2,7 @@
 
 import { twMerge } from "tailwind-merge";
 
-type Props = React.DetailedHTMLProps<
-  React.ButtonHTMLAttributes<HTMLButtonElement>,
-  HTMLButtonElement
->;
+type Props = React.ComponentPropsWithoutRef<"button">;
 
 export function TodoButton({ className, children, ...restProps }: Props) {
   return (
