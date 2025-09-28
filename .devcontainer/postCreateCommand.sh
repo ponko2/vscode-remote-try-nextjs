c777f03baf3a2425c8fe9ea0c417d8395b31f2eb
Takahito Nakano <kano@ponko2.jp>
2025-09-28 21:34:33 +0900

    chore: Clean up postCreateCommand.sh script

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -4,11 +4,5 @@ set -euo pipefail
 
 git config --global --add safe.directory /workspaces/vscode-remote-try-nextjs
 
-npm uninstall -g pnpm
-wget -qO- https://get.pnpm.io/install.sh | ENV="$HOME/.bashrc" SHELL="$(which bash)" bash -
-export PNPM_HOME="$HOME/.local/share/pnpm"
-export PATH="$PNPM_HOME:$PATH"
-pnpm config set store-dir "$HOME/.local/share/pnpm/store"
-
 pnpm install --force
 pnpm exec prisma migrate dev
